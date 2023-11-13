c2b2116ffdc6fe68eb666c2ee680d73d036c1dc3
Eric Zhu <ezhu5121@outlook.com>
2023-11-12 19:07:02 -0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::Read;
+
+use flate2::read::ZlibDecoder;
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,7 +19,27 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
         println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    } else if args[1] == "cat-file" {
+        if args[2] == "-p" {
+            let hash = &args[3];
+            let path = format!(".git/objects/{}/{}", &hash[..2], &hash[2..]);
+            let mut file = fs::File::open(path).unwrap();
+
+            let mut s = String::new();
+            let mut d = vec![0; 4096];
+            loop {
+                match file.read(&mut d) {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        let mut g = ZlibDecoder::new(&d[..n]);
+                        let _ = g.read_to_string(&mut s);
+                        print!("{}", s);
+                    }
+                    Err(err) => panic!("Error: {}", err),
+                }
+            }
+        } else {
+            println!("unknown option: {}", args[2])
+        }
     }
 }
