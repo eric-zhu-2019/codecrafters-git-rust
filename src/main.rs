72671762685126eefd7776943a4ae1b8e8ff4805
Eric Zhu <ezhu5121@outlook.com>
2023-11-12 19:09:03 -0800

    2nd pass

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ use flate2::read::ZlibDecoder;
 
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
