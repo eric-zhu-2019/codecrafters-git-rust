3172bcbcca4e2d3ea4fdc6b3fe0c147024b75ef5
Eric Zhu <ezhu5121@outlook.com>
2023-11-12 23:45:31 -0800

    cat file

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,48 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
-use std::io::Read;
+use std::io::BufReader;
 
+use clap::Command;
+use clap::arg;
 use flate2::read::ZlibDecoder;
 
+#[allow(dead_code)]
+fn deflat_file(path: &str) -> std::io::Result<()> {
+    let file = fs::File::open(path)?;
+    let mut out = std::io::stdout();
+    let mut reader = BufReader::new(file);
+    let mut g = ZlibDecoder::new(&mut reader);
+    std::io::copy(&mut g, &mut out)?;
+    Ok(())
+}
 
 fn main() {
 
-    // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else if args[1] == "cat-file" {
-        if args[2] == "-p" {
-            let hash = &args[3];
-            let path = format!(".git/objects/{}/{}", &hash[..2], &hash[2..]);
-            let mut file = fs::File::open(path).unwrap();
+    let mut appcmd = Command::new("mygit")
+        .subcommand(Command::new("init").about("init the git directory"))
+        .subcommand(Command::new("cat-file").about("cat object with hash")
+                    .arg(arg!(-p <HASH> "cat object with hash").required(true)));
 
-            let mut s = String::new();
-            let mut d = vec![0; 4096];
-            loop {
-                match file.read(&mut d) {
-                    Ok(0) => break,
-                    Ok(n) => {
-                        let mut g = ZlibDecoder::new(&d[..n]);
-                        let _ = g.read_to_string(&mut s);
-                        print!("{}", s);
-                    }
-                    Err(err) => panic!("Error: {}", err),
-                }
-            }
-        } else {
-            println!("unknown option: {}", args[2])
+    let cmds = appcmd.clone().get_matches();
+
+    match cmds.subcommand() {
+        Some(("init", _)) => { 
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+            println!("Initialized git directory");
+        }
+        Some(("cat-file", args)) => {
+            let hash = args.get_one::<String>("HASH").unwrap();
+            let path = format!(".git/objects/{}/{}", &hash[..2], &hash[2..]);
+            deflat_file(&path).unwrap();
+        }
+        _ => {
+            println!("No subcommand was used");
+            appcmd.print_help().unwrap();
         }
     }
+
 }
